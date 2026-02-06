f8f18adb1b4ce1062f93d40a1705beeb401acc89
TranVuAnhTuan-git <tranvuanhtuan160205@gmail.com>
2026-02-06 21:51:44 +0700

    Add files via upload

---
+++ src/main/java/com/aesp/backend/repository/SpeakingEvaluationRepository.java
@@ -0,0 +1,24 @@
+package com.aesp.backend.repository;
+
+import com.aesp.backend.entity.SpeakingEvaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface SpeakingEvaluationRepository extends JpaRepository<SpeakingEvaluation, Long> {
+
+        List<SpeakingEvaluation> findByLearnerIdOrderByCreatedAtDesc(Integer learnerId);
+
+        @Query("SELECT AVG(e.pronunciationScore), AVG(e.grammarScore), AVG(e.fluencyScore) " +
+                        "FROM SpeakingEvaluation e WHERE e.learnerId = ?1")
+        List<Object[]> getAverageScoresByLearner(Integer learnerId);
+
+        @Query(value = "SELECT u.full_name, l.pronunciation_score " +
+                        "FROM learner l " +
+                        "JOIN user u ON l.user_id = u.user_id " +
+                        "WHERE l.pronunciation_score IS NOT NULL " +
+                        "ORDER BY l.pronunciation_score DESC LIMIT 10", nativeQuery = true)
+        List<Object[]> getTopLearnersByPronunciation();
+}
